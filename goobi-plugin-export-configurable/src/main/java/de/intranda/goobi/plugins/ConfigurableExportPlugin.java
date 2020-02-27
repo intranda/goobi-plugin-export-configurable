27d0b6be751edfeedc8f95844fb41d82766458b7
Oliver Paetzel <oliver.paetzel@intranda.com>
2020-02-27 11:01:42 +0100

    allow configuration for projects and selection of ocr suffixes to export

--- goobi-plugin-export-configurable/src/main/java/de/intranda/goobi/plugins/ConfigurableExportPlugin.java
+++ goobi-plugin-export-configurable/src/main/java/de/intranda/goobi/plugins/ConfigurableExportPlugin.java
@@ -4,9 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.commons.configuration.SubnodeConfiguration;
 import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
 import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
 import org.apache.commons.lang.StringUtils;
 import org.goobi.beans.Process;
@@ -46,41 +51,44 @@ public class ConfigurableExportPlugin extends ExportDms implements IExportPlugin
     @Getter
     private String title = "intranda_export_configurable";
 
-    private String exportRootDirectory;
-    private boolean useSubFolderPerProcess;
-    private boolean includeDerivate;
-    private boolean includeMaster;
-    private boolean includeOcr;
-    private boolean includeSource;
+    public SubnodeConfiguration getConfig(Process process) {
+        String projectName = process.getProjekt().getTitel();
+        XMLConfiguration xmlConfig = ConfigPlugins.getPluginConfig(title);
+        xmlConfig.setExpressionEngine(new XPathExpressionEngine());
+        xmlConfig.setReloadingStrategy(new FileChangedReloadingStrategy());
 
-    private boolean includeImport;
-    private boolean includeExort;
-    private boolean includeITM;
-    private boolean includeValidation;
+        SubnodeConfiguration conf = null;
 
-    public ConfigurableExportPlugin() {
-        super();
-
-        XMLConfiguration config = ConfigPlugins.getPluginConfig(title);
-        config.setExpressionEngine(new XPathExpressionEngine());
-        exportRootDirectory = config.getString("/exportFolder", null);
-        useSubFolderPerProcess = config.getBoolean("/useSubFolderPerProcess", false);
-
-        includeDerivate = config.getBoolean("/folder/includeDerivate", false);
-        includeMaster = config.getBoolean("/folder/includeMaster", false);
-        includeOcr = config.getBoolean("/folder/includeOcr", false);
-        includeSource = config.getBoolean("/folder/includeSource", false);
-        includeImport = config.getBoolean("/folder/includeImport", false);
-        includeExort = config.getBoolean("/folder/includeExort", false);
-        includeITM = config.getBoolean("/folder/includeITM", false);
-        includeValidation = config.getBoolean("/folder/includeValidation", false);
-
+        // order of configuration is:
+        // 1.) project name matches
+        // 2.) project is *
+        try {
+            conf = xmlConfig
+                    .configurationAt("//config[./project = '" + projectName + "']");
+        } catch (IllegalArgumentException e) {
+            conf = xmlConfig.configurationAt("//config[./project = '*']");
+        }
+        return conf;
     }
 
     @Override
     public boolean startExport(Process process, String inZielVerzeichnis)
             throws IOException, InterruptedException, WriteException, PreferencesException, DocStructHasNoTypeException,
             MetadataTypeNotAllowedException, ExportFileException, UghHelperException, SwapException, DAOException, TypeNotAllowedForParentException {
+
+        SubnodeConfiguration config = getConfig(process);
+
+        String exportRootDirectory = config.getString("./exportFolder", null);
+        boolean useSubFolderPerProcess = config.getBoolean("./useSubFolderPerProcess", false);
+
+        boolean includeDerivate = config.getBoolean("./folder/includeDerivate", false);
+        boolean includeMaster = config.getBoolean("./folder/includeMaster", false);
+        boolean includeOcr = config.getBoolean("./folder/includeOcr", false);
+        boolean includeSource = config.getBoolean("./folder/includeSource", false);
+        boolean includeImport = config.getBoolean("./folder/includeImport", false);
+        boolean includeExort = config.getBoolean("./folder/includeExort", false);
+        boolean includeITM = config.getBoolean("./folder/includeITM", false);
+        boolean includeValidation = config.getBoolean("./folder/includeValidation", false);
 
         if (StringUtils.isBlank(exportRootDirectory)) {
             Helper.setFehlerMeldung("No export folder configured");
@@ -160,12 +168,17 @@ public class ConfigurableExportPlugin extends ExportDms implements IExportPlugin
             StorageProvider.getInstance().copyDirectory(masterFolder, Paths.get(destination.toString(), masterFolder.getFileName().toString()));
         }
         if (ocrFolder != null && Files.exists(ocrFolder)) {
+            Set<String> ocrSuffixes = new HashSet<>(Arrays.asList(config.getStringArray("./folder/ocr/suffix")));
             List<Path> ocrData = StorageProvider.getInstance().listFiles(ocrFolder.toString());
+
             for (Path path : ocrData) {
-                if (Files.isDirectory(path)) {
-                    StorageProvider.getInstance().copyDirectory(path, Paths.get(destination.toString(), path.getFileName().toString()));
-                } else {
-                    StorageProvider.getInstance().copyFile(path, Paths.get(destination.toString(), path.getFileName().toString()));
+                String suffix = getOcrPathSuffix(path);
+                if (ocrSuffixes == null || ocrSuffixes.size() == 0 || ocrSuffixes.contains(suffix)) {
+                    if (Files.isDirectory(path)) {
+                        StorageProvider.getInstance().copyDirectory(path, Paths.get(destination.toString(), path.getFileName().toString()));
+                    } else {
+                        StorageProvider.getInstance().copyFile(path, Paths.get(destination.toString(), path.getFileName().toString()));
+                    }
                 }
             }
         }
@@ -186,7 +199,7 @@ public class ConfigurableExportPlugin extends ExportDms implements IExportPlugin
         }
         if (validationFolder != null && Files.exists(validationFolder)) {
             StorageProvider.getInstance()
-            .copyDirectory(validationFolder, Paths.get(destination.toString(), validationFolder.getFileName().toString()));
+                    .copyDirectory(validationFolder, Paths.get(destination.toString(), validationFolder.getFileName().toString()));
         }
 
         // write mets file
@@ -197,6 +210,16 @@ public class ConfigurableExportPlugin extends ExportDms implements IExportPlugin
         Helper.setMeldung(null, process.getTitel() + ": ", "ExportFinished");
 
         return true;
+    }
+
+    public String getOcrPathSuffix(Path path) {
+        String name = path.getFileName().toString();
+        String suffix = "";
+        String[] suffixSplit = name.split("_");
+        if (suffixSplit != null && suffixSplit.length > 0) {
+            suffix = suffixSplit[suffixSplit.length - 1];
+        }
+        return suffix;
     }
 
 }
